593a3c65fb16a35e5cc4f343adb2b63d625b6be7
cosmin chauciuc <kosmin88@yahoo.com>
2016-11-21 11:15:55 +0200

    Add files via upload

---
+++ WpfApplication12/WpfApplication12/Connection.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
+using System.Windows;
+using System.Data;  
+using System.Data.SqlClient;
+using System.Windows.Controls;
+
+
+
+
+
+
+namespace WpfApplication12
+{
+    class Connection
+    {
+        //define variables
+        public string user;
+        public string pass;
+        public string host;
+        public string port;
+        public string service;
+        OracleConnection con;
+        public void OpenDb()
+        {
+            //create a new connection
+            con = new OracleConnection();
+            con.ConnectionString = "user id=" + user + ";password=" + pass + ";data source=" +
+                                    "(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)" +
+                                    "(HOST=" + host + ")(PORT=" + port + "))(CONNECT_DATA=" +
+                                     "(SERVICE_NAME=" + service + ")))";
+            //open connection
+            try
+            {
+                con.Open();
+                MessageBox.Show("Conectare cu succes");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+
+        }
+
+        public void CloseDb()
+        {
+            // Close and Dispose OracleConnection object
+            con.Close();
+            con.Dispose();
+            MessageBox.Show("Connection closed");
+        }
+
+        //public void Sql(string s)
+        public DataSet FillDataGrid(string s)
+        {
+            // Create the OracleCommand
+            OracleCommand cmd = new OracleCommand(s);
+            cmd.Connection = con;
+            cmd.CommandText = s;
+
+            // Execute command, create OracleDataReader object
+            // OracleDataReader reader = cmd.ExecuteReader();
+
+            DataSet ds = new DataSet();
+            try
+            {
+                OracleDataAdapter a = new OracleDataAdapter(cmd.CommandText, con);
+                a.Fill(ds);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+
+            return ds;
+
+            //  Dgv.DataSource(ds.Tables[0]);
+
+            // if (reader.HasRows)
+            //{
+            //get headerele si scrie un rand
+            //  var columns = Enumerable.Range(0, reader.FieldCount).Select(reader.GetName).ToList();
+            //var columns = Enumerable.Range(0, 4).Select(reader.GetName).ToList();
+
+            /* foreach (string coloana in columns)
+                 MessageBox.Show(coloana + " ");
+                
+
+             while (reader.Read())
+             {
+                 object[] values = new object[reader.FieldCount];
+                 //object[] values = new object[4];
+
+                 int numColumns = reader.GetValues(values); //after "reading" a row
+
+                 for (int i = 0; i < numColumns; i++)
+                 {
+                     MessageBox.Show(values[i] + " ");
+                 }
+                    
+             }
+             */
+
+
+
+
+            // }
+        }
+        // Dispose OracleDataReader object
+        //  reader.Dispose();
+
+        // Dispose OracleCommand object
+        // cmd.Dispose();
+        
+        public  string Sql(string s)
+        {
+            // Create the OracleCommand
+            OracleCommand cmd = new OracleCommand(s);
+            cmd.Connection = con;
+            cmd.CommandText = s;
+
+            // Execute command, create OracleDataReader object
+            try
+            {
+                OracleDataReader reader = cmd.ExecuteReader();
+            
+            if (reader.HasRows)
+            {
+                StringBuilder builder = new StringBuilder();
+
+                //get headerele si scrie un rand
+                var columns = Enumerable.Range(0, reader.FieldCount).Select(reader.GetName).ToList();
+                //var columns = Enumerable.Range(0, 4).Select(reader.GetName).ToList();
+
+                foreach (string coloana in columns)
+                {
+                    builder.Append(coloana);
+                    builder.Append(' ');
+                }
+               builder.Append("\n");
+                
+                while (reader.Read())
+                {
+                    object[] values = new object[reader.FieldCount];
+                    //object[] values = new object[4];
+
+                    int numColumns = reader.GetValues(values); //after "reading" a row
+
+                    for (int i = 0; i < numColumns; i++)
+                    {
+                        builder.Append(values[i]);
+                        builder.Append(' ');
+
+                    }
+                    builder.Append("\n");
+
+                }
+
+
+                return builder.ToString();  
+
+            }
+            else
+            {
+                int numberOfRecords = cmd.ExecuteNonQuery();
+                return numberOfRecords.ToString() + " rows updated";
+
+            }
+                 reader.Dispose();
+
+            // Dispose OracleCommand object
+            cmd.Dispose();
+            }
+            catch (Exception e)
+                {
+                    return e.ToString();
+                }
+           
+           }
+
+           // return "succes";
+                // Dispose OracleDataReader object
+            
+        }
+
+    }
+
---
+++ WpfApplication12/WpfApplication12/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel; 
+ 
+
+namespace WpfApplication12
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // test connection to database
+            Connection instanta = new Connection();
+            instanta.user = UserTxt.Text;
+            instanta.pass = PassTxt.Text;
+            instanta.host = HostTxt.Text;
+            instanta.port = PortTxt.Text;
+            instanta.service = ServiceTxt.Text;
+            instanta.OpenDb();
+            instanta.CloseDb();
+
+            
+           // instanta.CloseDb();
+        }
+
+        public void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //create a new connection
+            string x = SqlTxt.Text;
+            Connection instanta = new Connection();
+            instanta.user = UserTxt.Text;
+            instanta.pass = PassTxt.Text;
+            instanta.host = HostTxt.Text;
+            instanta.port = PortTxt.Text;
+            instanta.service = ServiceTxt.Text;
+           // if sql text starts with select take to datagrid
+            instanta.OpenDb();
+            if (x.Substring(0,6)=="select")
+            {
+            try
+            {
+                dataGridView1.ItemsSource = instanta.FillDataGrid(x).Tables[0].DefaultView;
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.ToString());
+            }
+            }
+
+            else {
+                //else update sqltxt 
+                MessageBox.Show("Only Select commands");
+                SqlTxt.Text = instanta.Sql(x);
+            }
+                 //instanta.Sql(x);
+            instanta.CloseDb();
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //create a new object excel application
+            Excel.Application xlapp = new Excel.Application();
+            xlapp.Visible = true;
+            //new workbook and new worksheet
+            Excel.Workbook wb;
+            Excel.Worksheet ws;
+            object misValue = System.Reflection.Missing.Value;
+           
+            //assign object
+           wb = xlapp.Workbooks.Add(misValue);
+           ws = wb.Worksheets.get_Item(1);
+           //ws.Cells[1,1] = "kosmin";
+
+           copyAlltoClipboard();
+
+           Excel.Range CR = (Excel.Range)ws.Cells[1, 1];
+           CR.Select();
+           ws.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+           Excel.Range cr1 = ws.Cells[1, 1];
+           cr1.Select();
+          
+        }
+
+        //copy to clipboard all data from gridview
+        private void copyAlltoClipboard()
+        {
+            dataGridView1.SelectAll();
+
+            dataGridView1.ClipboardCopyMode = DataGridClipboardCopyMode.IncludeHeader;
+            ApplicationCommands.Copy.Execute(null, dataGridView1);
+            dataGridView1.UnselectAllCells();
+
+            
+        }
+
+        
+    
+
+    
+    }
+}
